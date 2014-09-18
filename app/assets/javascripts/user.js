2027c05b5ba639bae3e079e19d585deba72f3de4
Stephanie-Chou <stephanie.chou@outlook.com>
2014-09-17 21:16:38 -0500

    nav bar

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -7,4 +7,8 @@ $(document).ready(function(){
 		var password = $('#password').val();
 		request = $.post("users/login", user={email: email, password: password});
 	});
+	$("#logout").on("click", function(e){
+		e.preventDefault();
+			request = $.post("users/logout");
+	});
 });
