d7e6c35bc5050888d319064292cda47be2e2a2cf
Stephanie-Chou <stephanie.chou@outlook.com>
2014-10-01 12:40:09 -0500

    loginlogout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
-
+  include SessionHelper
   #Get form for new user
   def new
     @user = User.new
   end
 
+  def profile
+    @user = current_user
+    @recommendations = Recommendation.where(recFor: @user)
+    @friend_requests = User.find_friend_requests(@user)
+  end
+
+  def logout
+    p "*"*100
+    session.clear
+    if request.xhr?
+      p "logout xhr"
+      redirect_to root_path
+      # render :json => {status: 200}
+    else
+      p "logout redirect "*100
+      redirect_to root_path
+    end
+  end
   #post for new user
   def create
     @user = User.new(user_params)
@@ -17,37 +35,32 @@ class UsersController < ApplicationController
   end
 
   def login
-  	@user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
+    p "*"*100
+  	@user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
+      p session[:user_id]
+      if request.xhr?
+      p "xhr "*20
+      render :json => Recommendation.where(recFor: @user).to_json(:include => [:restaurant, :recBy])
+      else
+        p "login redirect "*10
+        redirect_to profile_path
+      end
     else
       if request.xhr?
-        render :json => {message: "fail"}
+        p "xhr "*20
+        render :json => {error: '404'}
       else
+        p "root "*20
         redirect_to root_path
       end
     end
 
-    if request.xhr?
-      p "login xhr"
-    	render :json => Recommendation.where(recFor: @user).to_json(:include => [:restaurant, :recBy])
-    else
-      p "login redirect"
-    	redirect_to root_path
-    end
+    
   end
 
-  def logout
-    
-    session[:user_id] = nil
-    if request.xhr?
-      p "logout xhr"
-      render :json => {status: 200}
-    else
-      p "logout redirect"
-      redirect_to root_path
-    end
-  end
+
 
   private
 
